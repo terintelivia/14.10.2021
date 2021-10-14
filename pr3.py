03993482dbff3d987563a204ff076e8cdda10882
terintelivia <70704787+terintelivia@users.noreply.github.com>
2021-10-14 10:10:50 +0300

    Add files via upload

---
+++ pr3.py
@@ -0,0 +1,8 @@
+print('Ce nume si prenume ai?')
+np=str(input('Introdu numele si prenumele:'))
+print(np)
+print('Ce iti doresti sa devii in viitor?')
+pr=str(input('Inrodu o profesie:'))
+print(pr)
+print('Te numesti',np)
+print('Doresti sa devii',pr)
